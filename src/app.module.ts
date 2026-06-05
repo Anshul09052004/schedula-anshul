775f2aa4cff5c27b21eade6e6ff7045dc482b37c
Anshul prajapat <anshulprajapat982@gmail.com>
2026-06-05 16:23:10 +0530

    Implemented role based authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+
+import { DoctorController } from './doctor/doctor.controller';
+import { PatientController } from './patient/patient.controller';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    JwtModule.register({
+      secret: 'schedula-secret-key',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [
+    AppController,
+    AuthController,
+    DoctorController,
+    PatientController,
+  ],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
